e85b9a2cec21ed8044d9e5c21779ad349faf1c0f
Patrik Johnson <patrik@zebros.fi>
2016-07-26 18:34:30 +0300

    Minor testing based on rollup setup branch

--- bouncycastle.js
+++ bouncycastle.js
@@ -4,12 +4,20 @@ let m = require('mithril');
 let server =  new Pretender();
 
 server.handledRequest = function(verb, path, request) {
-  console.log('caught ?{ verb } ?{ path }');
+  console.log(`caught ${ verb } ${ path }`);
 };
 
 server.unhandledRequest = function(verb, path, request) {
-  console.log('uncaught ?{ verb } ?{ path }: ', request);
+  console.log(`uncaught ${ verb } ${ path }:`, request);
 };
 
 console.log(Pretender);
 
+function reqListener () {
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "http://www.example.org/example.txt");
+oReq.send();
